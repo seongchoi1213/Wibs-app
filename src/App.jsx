901db7a420d5406c036f2f0adf1edf9f350355b2
seongchoi1213 <anycall9654@gmail.com>
2026-05-14 10:05:04 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { db } from "./firebase";
 import { collection, addDoc, onSnapshot, query, orderBy, getDocs, updateDoc, doc, deleteDoc } from "firebase/firestore";
 const INIT_USERS = [
